4be9d98f9fa3213194a2ff6f727ef46ec7fe35d4
yyNoBug <yaoyuan2000@sjtu.edu.cn>
2019-05-01 18:11:09 +0800

    Return.hpp has been finished, but has not been debugged yet.

--- return.hpp
+++ return.hpp
@@ -8,7 +8,6 @@ struct time{
     short data;
 
     time():data(0){}
-
     time(char[] s){
         if (char[0] == 'x') data = -1;
         else data = (((s[0]-'0')*10 + s[1]-'0')*10 + s[3]-'0')*10 + s[4]-'0';
@@ -16,7 +15,6 @@ struct time{
 
     short hour(){return (data / 100);}
     short minute(){return (data % 100);}
-
 };
 
 struct date{
@@ -32,13 +30,21 @@ struct date{
     }
 };
 
-These above are written in flow.hpp
+These above are written in flow.hpp, which I haven't pushed
 */
 
-struct query_profile_return{
+struct query_profile_return {
     wchar_t name[20];
     char email[20], phone[20];
     short privilege;
+
+    query_profile_return (wchar_t _name[], char _email[], char _phone[],
+                         short _privilege){
+        memcpy(name, _name, 20);
+        memcpy(email, _email, 20);
+        memcpy(phone, _phone, 20);
+        privilege = _privilege;
+    }
 };
 
 struct info_ticket{
@@ -51,17 +57,44 @@ struct info_ticket{
 
 struct query_ticket_return{
     int listnum;
-    info_ticket data[]; //it hasn't been finished
+    info_ticket* data;
+
+    query_ticket_return(int _listnum, const info_ticket& _data[]){
+        listnum = _listnum;
+        data = new info_ticket[listnum];
+        memcpy(data, _data, listnum);
+    }
+
+    ~query_order_return(){
+        delete [] data;
+    }
 };
 
 struct query_transfer_return{
-    bool sucess;
+    bool success;
     info_ticket data[2];
+
+    query_transfer_return(bool _success, info_ticket _data[]){
+        success = _success;
+        memcpy(data, _data, 2);
+    }
 };
 
 struct query_order_return{
     bool success;
-    info_ticket data[]; //it hasn't been finished
+    short listnum;
+    info_ticket* data;
+
+    query_order_return(bool _success, short listnum, info_ticket _data[]){
+        success = _success;
+        listnum = _listnum;
+        data = new info_ticket[listnum];
+        memcpy(data, _data, listnum);
+    }
+
+    ~query_order_return(){
+        delete [] data;
+    }
 };
 
 struct info_station{
@@ -71,8 +104,27 @@ struct info_station{
 };
 
 struct query_train_return{
+    short num_station, num_price;
     wchar_t name[20], name_price[5][20];
     char train_id[20], catalog[10];
-    short num_station, num_price;
-    info_station data[]; //it hasn't been finished
+    info_station* data;
+
+    query_train_return(wchar_t _name[], wchar_t _name_price[][20],
+                       char _train_id[], char _catalog[], short _num_station,
+                       short _num_price, info_station _data[]){
+        num_price = _num_price;
+        num_station = _numstation;
+        memcpy(name, _name, 20);
+        for (int i = 0; i < num_price; ++i){
+            memcpy(name_price[i], _name_price[i], 20);
+        }
+        memcpy(train_id, _train_id, 20);
+        memcpy(catalog, _catalog, 10);
+        data = new info_station[listnum];
+        memcpy(data, _data, listnum);
+    }
+
+    ~query_train_return(){
+        delete [] data;
+    }
 };
